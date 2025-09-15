1dd8cec91a7208ea80469480321a40050908ce26
0520MZ <matthewjzhu0520@gmail.com>
2025-09-15 01:37:19 -0400

    Basic setup for an instantiable carousel

---
+++ src/app/carousel.tsx
@@ -0,0 +1,7 @@
+export type carouselItem{
+     id: string;
+     image: string;
+     title: string;
+     description: string;
+     //href: string;   unused for now. maybe future linking?
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client";         //Tells next.js to treat this file as a client component - meaning that it will run in the browser.
+import {carouselItem} from './carousel';    
+
 export default function Home() {
   return (
     <main className="min-h-screen pt-20">
       <div className="container mx-auto relative">
-        <h1 className="text-7xl font-extrabold text-white  
+        <h1 className="text-6xl md:text-7xl font-extrabold text-white ml-auto mb-4  
         animate-slide-up [--delay:.1s]
         [--glow:drop-shadow(0_0_17px_#828282)_drop-shadow(0_0_5px_#828282)]">Welcome to my website.</h1>
-        <br></br>
         <br></br>
         <p className="text-2xl font-bold text-light-gray mb-12 animate-slide-up [--delay:.15s]">
           Here you will find some of my projects that I have been working on.
